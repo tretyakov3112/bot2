8f566a9377f7fa2fadc04e4dc90132d89b1e2c39
Spyteamalex __ <spyteamalex@gmail.com>
2022-03-16 19:49:46 +0300

    Гуманитарная помощь

--- demo/src/main/java/com/example/demo/Bot2Application.java
+++ demo/src/main/java/com/example/demo/Bot2Application.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.ApiContextInitializer;
 
 
 @SpringBootApplication
 public class Bot2Application {
 
 	public static void main(String[] args) {
-		ApiContextInitializer.init();
 		SpringApplication.run(Bot2Application.class, args);
 	}
 
